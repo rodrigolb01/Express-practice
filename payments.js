bc8ec58120b96903b9bc1ebac2225cb8d35f0268
rodrigolb01 <rodrigolb01@gmail.com>
2023-01-12 18:20:44 -0300

    first commit

---
+++ payments.js
@@ -0,0 +1,102 @@
+const express = require('express');
+
+const router = express.Router();
+
+const suppliers = [
+    {
+        id: 0,
+        name: "Alpha Networking",
+        value: 190.00
+    },
+    {
+        id: 1,
+        name: "Carrefour",
+        value: 199.00
+    },
+    {
+        id: 2,
+        name: "Fire Base",
+        value: 89.00
+    }
+];
+
+const employees = [
+    {
+        firstName : "John",
+        lastName : "Doe",
+        value: 202.00
+    },
+    {
+        firstName : "Craig",
+        lastName : "Anderson",
+        value: 200.50
+    },
+    {
+        firstName : "Tom",
+        lastName : "Weasley",
+    },
+    {
+        firstName : "Andrew",
+        lastName : "Walker",
+        value: 190.00
+    }
+];
+
+router.get("/suppliers",(req, res) => {
+    res.render("paycheck_suppliers", { paycheck: suppliers})
+})
+
+router.post("/suppliers/new",(req, res) => {
+    res.send("adding new payment for employee")
+})
+
+router.get("/suppliers/:id",(req, res) => {
+    res.send(`supplier payment with Id ${req.params.id}`)
+})
+
+router.put("/suppliers/:id", (req, res) => {
+    res.send(`editing supplier payment with Id ${req.params.id}`)
+})
+
+router.delete("/suppliers/:id", (req, res) => {
+    res.send(`deleting supplier payment with Id ${req.params.id}`)
+})
+
+router.get("/employees",(req, res) => {
+    res.render("paycheck_employees",{ paycheck: employees})
+})
+
+router.param("id", (req, res, next, id) => {
+    req.users = users[id]
+    next();
+})
+
+router.post('/employees/', (req, res) => {
+    //if isvalid
+    console.log("adding:");
+    employees.push(
+        {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            value: req.body.val
+        })
+    res.send(`first name: ${req.body.firstName}, lastName: ${req.body.lastName}, value: ${req.body.val}`);
+})
+
+router.get('/employees/new', (req, res) => {
+    res.render("paycheck_employees_add");
+})
+
+router.get('/employees/:id', (req, res) => {
+    res.send(`Employee with id ${req.params.id}`)
+})
+
+router.put('/employees/:id', (req, res) => {
+    res.send(`Editing employee with id ${req.params.id}`);
+})
+
+router.delete('/employees/:id', (req, res) => {
+    res.send(`Deleting employee with id ${req.params.id}`);
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const payments = require('./payments');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(logger);
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.get("/download",(req, res) => {
+    console.log("Here");
+    // res.download("virus.txt");
+    res.render("download", {text: "1234"})
+})
+
+//router for payments crud
+app.use('/payments', payments);
+
+//middleware takes a function to execute next
+function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+}
+
+//http://localhost:3000/
+app.listen(3000);
